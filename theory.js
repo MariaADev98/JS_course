41e707484b0b38b2842747838301039618d3faad
MariaADev98 <bulatova2798@gmail.com>
2025-02-14 10:12:01 +0300

    13-14.02

---
+++ theory.js
@@ -0,0 +1,48 @@
+//===============operators
+let a = 10
+let b = 20
+let c = 30
+
+// c +=a
+// c -=a
+// c *=a
+// c /=a
+
+//c = c **2 //возведение в степень
+
+//================Data types
+const age = 29
+const name = 'Maria'
+const isProgramm = true 
+let x //undefined\
+//null - отдельный тип данных, через typeof определяется как object 
+// console.log(typeof(1 / x))
+// console.log(typeof NaN)
+
+
+
+//===============Priority
+const fullAge = 26
+const birthYear = 1998
+const currentYear = 2024
+// > < >=  <= 
+const ifFullAge = currentYear- birthYear >= fullAge
+//console.log(ifFullAge)
+const num1 = 42
+const num2 = '42'
+console.log(num1 === num2)//тут мы сравниваем ТОЛЬКО ЗНАЧЕНИЯ 42(если ==(двойное равно))
+/// ===(тройное равно) сранивает и значение и тип данных 
+
+
+// =====================Array 
+
+// //Theory
+
+// const arr = [1, 2, 3, 5, 20, 42]
+
+// console.log(arr)
+
+// console.log(arr[arr.length - 1]) //узнаем последний элемент массива 
+// arr[0] = 'Hello'// изменить значение массива 
+// console.log(arr)
+
